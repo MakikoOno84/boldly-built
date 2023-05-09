011b1f7f6cc347ca37d3486a5a3a9c3b1e2faf85
Josy Li <josyliyh@gmail.com>
2023-05-08 20:47:26 -0700

    added section title on frontpage

--- front-page.php
+++ front-page.php
@@ -16,20 +16,12 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+		<section id="home-expertise">
+			<?php if ( get_field('home_section_title_') ) : ?>
+				<h2><?php the_field('home_section_title_')?></h2>
+			<?php endif ?>
+		</section>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
 
 	</main><!-- #main -->
 
